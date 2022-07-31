40827ad93d4c95f223b1b58897def22a691aaf77
onifernando1 <onifernando1@gmail.com>
2022-07-31 17:20:44 +0100

    Print board 8 x 8 blue squares

--- lib/ruby.rb
+++ lib/ruby.rb
@@ -1,13 +1,22 @@
 require 'colorize'
-
+#current board logic 
+# make grid 1..100
+# odd numbers replace with white square
+# even numbers replace with black square
+# join together 
+# print 
 
 class Board
     def initialize 
-        @board = Array.new(8,Array.new(8," "))
+        white_square = "  ".colorize(background: :blue)
+        black_square = "  ".colorize(background: :red)
+
+        @board = Array.new(8, Array.new(8,square))
     end 
 
     def show_board
-        p @board
+        @board = @board.map {|array| array.join("")}
+        puts @board 
     end 
 
 
@@ -30,3 +39,21 @@ puts knight.piece
 board = Board.new()
 board.show_board()
 puts "Hello".blue
+puts "  ".colorize(background: :blue)
+
+
+
+
+array = Array.new(8, Array.new(8, "O"))
+array.each do |item|
+    new_array = item.flatten
+    puts new_array 
+end 
+ 
+
+array = [[1, 2, 3, 4],[1,2,3,4],[1,2,3,4]]
+a = [1, 2, 3 , 4]
+a = a.join()
+puts a 
+
+puts array.map {|item| item.join("")}
