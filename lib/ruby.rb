6482ad6b99ef41fcfc2e37e8f433f91dbe22deb9
onifernando1 <onifernando1@gmail.com>
2022-07-31 20:39:12 +0100

    Colour board function

--- lib/ruby.rb
+++ lib/ruby.rb
@@ -8,11 +8,42 @@ require 'colorize'
 
 class Board
     def initialize 
-        white_square = "  ".colorize(background: :blue)
-        black_square = "  ".colorize(background: :red)
+        @white_square = "  ".colorize(background: :blue)
+        @black_square = "  ".colorize(background: :red)
 
-        @board = Array.new(8, Array.new(8,square))
+        @board = Array.new(8, Array.new(8,nil))
     end 
+
+    def colour_board
+
+        odd = [1,3,5,7]
+        even = [0,2,4,6]
+
+        @board.each_with_index do |data, index|
+            puts "Index #{index}"
+            puts "Data #{data}"
+            if index == 0 || index == 2 || index == 4 || index == 6 
+                for i in odd 
+                    @board[index][i] = @white_square
+                end 
+                for i in even 
+                    @board[index][i] = @black_square
+                end 
+            end 
+            if index == 1 || index == 3 || index == 5 || index == 7 
+                for i in even 
+                    @board[index][i] = @white_square
+                end 
+                for i in odd 
+                    @board[index][i] = @black_square
+                end 
+            end 
+        end 
+
+        @board
+
+    end 
+
 
     def show_board
         @board = @board.map {|array| array.join("")}
@@ -35,25 +66,28 @@ class Knight
 end
 
 knight = Knight.new()
-puts knight.piece
-board = Board.new()
-board.show_board()
-puts "Hello".blue
-puts "  ".colorize(background: :blue)
+# puts knight.piece
+# board = Board.new()
+# board.show_board()
+# puts "Hello".blue
+# puts "  ".colorize(background: :blue)
 
 
 
 
-array = Array.new(8, Array.new(8, "O"))
-array.each do |item|
-    new_array = item.flatten
-    puts new_array 
-end 
+# array = Array.new(8, Array.new(8, "O"))
+# array.each do |item|
+#     new_array = item.flatten
+#     puts new_array 
+# end 
  
 
-array = [[1, 2, 3, 4],[1,2,3,4],[1,2,3,4]]
-a = [1, 2, 3 , 4]
-a = a.join()
-puts a 
+# array = [[1, 2, 3, 4],[1,2,3,4],[1,2,3,4]]
+# a = [1, 2, 3 , 4]
+# a = a.join()
+# puts a 
 
-puts array.map {|item| item.join("")}
+# puts array.map {|item| item.join("")}
+board = Board.new()
+board.colour_board()
+
