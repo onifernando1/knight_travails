e3f68e4cc7402539f6c7cb1d30656f012a3adc6a
onifernando1 <onifernando1@gmail.com>
2022-07-31 22:05:49 +0100

    Make board, colour board, show board function

--- lib/ruby.rb
+++ lib/ruby.rb
@@ -23,36 +23,50 @@ class Board attr_accessor :board
         @board = Array.new(rows) do
             Array.new(columns) { "a" }
         end
+    end 
 
-        p @board
 
-        @board[0][1] = "z"
-         
+    def colour_board
+
+        odd = [1,3,5,7]
+        even = [0,2,4,6]
+        
+        @board.each_with_index do |item, index|
+        if index == 0 || index == 2 || index == 4 || index == 6
+            for i in odd 
+            p @board[index][i] = @white_square
+            end 
+        end 
+
+        if index == 0 || index == 2 || index == 4 || index == 6
+            for i in even 
+            p @board[index][i] = @black_square
+            end 
+        end 
+
+        if index == 1 || index == 3 || index == 5 || index == 7
+            for i in odd 
+            p @board[index][i] = @black_square
+            end 
+        end 
+
+        if index == 1 || index == 3 || index == 5 || index == 7
+            for i in even 
+            p @board[index][i] = @white_square
+            end 
+        end 
+
         p @board
     end 
 
 
-#     def colour_board
-
-#         odd = [1,3,5,7]
-#         even = [0,2,4,6]
-#         y = ["a","a","a","a","a","a","a","a"]
-#         @board = @board.map do |item|
-#             if item == "a"
-#                 item = y
-#             end 
-#         end  
-
-#         @board[0][1] = "z"
-#         p @board 
-
-#     end 
+    end 
 
 
-#     def show_board
-#         @board = @board.map {|array| array.join("")}
-#         puts @board 
-#     end 
+    def show_board
+        @board = @board.map {|array| array.join("")}
+        puts @board 
+    end 
 
 
 
@@ -72,17 +86,5 @@ end
 # knight = Knight.new()
 board = Board.new()
 board.make_board()
-# board.colour_board()
-
-# rows = 8 
-# columns = 8
-
-# board = Array.new(rows) do
-#     Array.new(columns) { "a" }
-#   end
-
-#   p board 
-
-#   board[0][1] = "a"
-
-#   p board
+board.colour_board()
+board.show_board()
