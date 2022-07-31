b0313f1e5262a8f41e1f27e20b0c1096eb0c37d4
onifernando1 <onifernando1@gmail.com>
2022-07-31 22:26:32 +0100

    Make board rectangular to centralize knight

--- lib/ruby.rb
+++ lib/ruby.rb
@@ -1,69 +1,101 @@
-# frozen_string_literal: true
-
 require 'colorize'
 
-class Board
-  def initialize
-    @white_square = '  '.colorize(background: :white)
-    @black_square = '  '.colorize(background: :black)
-    make_board
-  end
+class Board attr_accessor :board
+    
+    
+    def initialize 
+        @white_square = "   ".colorize(background: :white)
+        @black_square = "   ".colorize(background: :black)
+        self.make_board()
+        
+    end 
 
-  def make_board
-    rows = 8
-    columns = 8
+    def make_board
 
-    @board = Array.new(rows) do
-      Array.new(columns) { 'a' }
-    end
-  end
+        rows = 8 
+        columns = 8
 
-  def colour_board
-    odd = [1, 3, 5, 7]
-    even = [0, 2, 4, 6]
-
-    @board.each_with_index do |_item, index|
-      if [0, 2, 4, 6].include?(index)
-        odd.each do |i|
-          @board[index][i] = @black_square
+        @board = Array.new(rows) do
+            Array.new(columns) { "a" }
         end
-      end
+    end 
 
-      if [0, 2, 4, 6].include?(index)
-        even.each do |i|
-          @board[index][i] = @white_square
-        end
-      end
 
-      if [1, 3, 5, 7].include?(index)
-        odd.each do |i|
-          @board[index][i] = @white_square
-        end
-      end
+    def colour_board
 
-      next unless [1, 3, 5, 7].include?(index)
+        odd = [1,3,5,7]
+        even = [0,2,4,6]
+        
+        @board.each_index do |index|
+        if index == 0 || index == 2 || index == 4 || index == 6
+            for i in odd 
+            @board[index][i] = @black_square
+            end 
+        end 
 
-      even.each do |i|
-        @board[index][i] = @black_square
-      end
-    end
-  end
+        if index == 0 || index == 2 || index == 4 || index == 6
+            for i in even 
+            @board[index][i] = @white_square
+            end 
+        end 
 
-  def show_board
-    @board = @board.map { |array| array.join('') }
-    puts @board
-  end
-end
+        if index == 1 || index == 3 || index == 5 || index == 7
+            for i in odd 
+            @board[index][i] = @white_square
+            end 
+        end 
+
+        if index == 1 || index == 3 || index == 5 || index == 7
+            for i in even 
+            @board[index][i] = @black_square
+            end 
+        end 
+
+
+        @string = " \u265E " 
+        @piece =  @string.encode("utf-8") 
+
+        @board[7][1] = @piece.colorize(background: :yellow)
+
+
+    end 
+
+
+    end 
+
+
+    def show_board
+        @board = @board.map {|array| array.join("")}
+        puts @board 
+    end 
+
+    def practice
+    
+    end 
+
+end 
 
 class Knight
-  attr_accessor :piece
 
-  def initialize
-    @string = "\u2658"
-    @piece = @string.encode('utf-8')
-  end
+    attr_accessor :piece
+
+    def initialize 
+        @string = "\u2658"
+        @piece =  @string.encode("utf-8")
+    end 
+
+    def move(y, x)
+
+    end 
+
+    def practice
+
+    end 
+
 end
 
-board = Board.new
-board.colour_board
-board.show_board
+board = Board.new()
+board.colour_board()
+board.show_board()
+knight = Knight.new()
+board.practice()
